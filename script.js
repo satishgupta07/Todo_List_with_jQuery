0055528811553f5dbfe698a8d420abd69f6ff3f7
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-01 20:23:42 +0530

    Toggle done/not-done for items
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ let ulTasks = $('#ulTasks')
 let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask = $('#inpNewTask')
-
 btnAdd.click(() => {
   let listItem = $('<li>', {
     'class': 'list-group-item',
     text: inpNewTask.val()
   })
+  listItem.click(() => {
+    listItem.toggleClass('done')
+  })
   ulTasks.append(listItem)
   inpNewTask.val('')
 })
-
 btnClear.click(() => inpNewTask.val(''))
 
